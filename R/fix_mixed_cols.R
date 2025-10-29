ff5544c3860b5ed86fee37465f5ba359a3ebab71
Andres Flores <andres.flores@uaslp.mx>
2025-10-29 15:52:41 -0600

    Fixed documentation

--- R/fix_mixed_cols.R
+++ R/fix_mixed_cols.R
@@ -1,22 +1,38 @@
 #' @importFrom purrr map_chr
+#' @importFrom dplyr mutate
+#' @importFrom dplyr across
+#' @importFrom dplyr where
 #' @importFrom readr type_convert
-#' Fix mixed cols in dataframes
+#' @name fix_mixed_cols
+#' @title Fix mixed cols in dataframes
 #'
 #' @param df Dataframe to fix
 #'
 #' @returns A dataframe with columns with columns without list
 #'
 #' @examples
-#' df <- data.frame
+#'
+#' col_1 <- c(1, 3, 4, 5, 5, 2)
+#' col_2 <- c(1, 2, 3, 4, 5, 6)
+#' col_3 <- c("a", "b", "c", "d", "e", "f")
+#'
+#'
+#' df <- data.frame(col_1, col_2, col_3)
+#'
+#' df$col_1 <- as.list(df$col_1) # Reproduce an importing error
+#'
+#' str(df)
 #'
 #' fixed_df <- fix_mixed_cols(df)
+#' str(fixed_df)
+#'
 #'
 #' @export
-fix_mixed_cols <- function(df) {
+fix_mixed_cols <- function(df){
   df %>%
-    mutate(
-      across(
-        where(is.list),
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::where(is.list),
         ~ purrr::map_chr(.x, function(v) {
           # NA o vacío -> NA_character_
           if (length(v) == 0 || all(is.na(v))) return(NA_character_)
